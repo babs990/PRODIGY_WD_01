4774bae8bed4fe55408a3c9d99171daefa779590
babacar <nguiranebabacar305@gmail.com>
2025-01-10 00:49:50 +0000

    new

--- src/script.js
+++ src/script.js
@@ -27,7 +27,22 @@ window.onload = ()=>{
         opacity:0
     })
 }
-tanjiro = document.querySelector('#tanjiro2')
+
+tanjiro = document.querySelector('#nezuko2')
 tanjiro.addEventListener('click',()=>{
-    tanjiro.querySelector(img).getAttribute(src) = 'nezuko.png'
+    document.querySelector('#tanjiro').querySelector('img').setAttribute('src','images/nezuko.png')
+    gsap.from('#tanjiro',{
+        translateX :200,
+        duration :2.5,
+        opacity :0,
+        ease : 'elastic'
+    })
+    document.querySelector('#accroche').setAttribute('style','color: #A56388;')
+    document.querySelector('#nezuko2').setAttribute('style','border-color: #A56388;')
+    document.querySelector('nav').setAttribute('style','border-color: #a5638844;')
+    document.querySelector('#logo').setAttribute('src','images/nezuko.png')
+
+    if(document.querySelector('#tanjiro2').classList.contains('border-[#75AF82]')){
+        document.querySelector('#tanjiro2').classList.remove('border-[1px]')
+    }
 })
